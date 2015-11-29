eea7fe16d08aafed2a1e1d2dbc1cbc9b50b4b64f
NatalieBlack <natalievictoriablack@gmail.com>
2015-11-29 00:31:26 -0500

    remove load memories callback from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,11 @@ class User < ActiveRecord::Base
   has_many :tweets, :dependent => :destroy
   has_many :tumblr_posts, :dependent => :destroy
 
-  after_create :load_memories
-  
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
   validates_uniqueness_of :twitter_name, :tumblr_url
 
   def memories
     tweets + tumblr_posts
-  end
-
-  def load_memories
-    Rails.logger.info "importing memories for #{name}"
-    Memory.history_for_user(self)
-    Rails.logger.info "importing memories for #{name}"
   end
 
   def email_verified?
