4ba3924b88c678b7343db8b84ef6b7cc2496db00
NatalieBlack <natalievictoriablack@gmail.com>
2015-11-30 00:40:00 -0500

    initial front end logic for latest import

--- app/assets/javascripts/memories.js
+++ app/assets/javascripts/memories.js
@@ -1,29 +1,47 @@
 $(document).on('ready page:load', function() {
   $('.generate_memories').on('submit', function(e) {
     e.preventDefault();
-    $('#initial_loading').show();
+    var form = this;
+
+    $('#initial_loading').fadeIn();
+
     $('.update').hide();
     $('.update').text('This could take a while.');
     $('.update').delay(3000).fadeIn();
-    var form = $(this)[0];
 
     $.post(form.action, {"source":  form.source.value}, function(data) {
       $('.update').fadeOut();
+
       $('#initial_loading').fadeOut(function(){
         $('.update').text(data['memories_count'] + " memories imported");
         $('.update').fadeIn();
+
         if(data['memories_count'] > 0) {
           $(form).children('input[type=submit]').prop('disabled', true);
         }
+
       });
     });
 
   });
 
-  $('#import_latest').on('submit', function() {
-    $(this).hide();
-    $('#latest_loading').show();
+  $('#import_latest').on('submit', function(e) {
+    e.preventDefault();
+    var form = this;
+
+    $(this).fadeOut(function() {
+      $('#latest_loading').css({"display": "inline-block"});
+      $('#latest_loading').fadeIn();
+    });
+
+
     $.post(form.action, function(data) {
+
+      $('#latest_loading').fadeOut(function() {
+        $('#complete').css({"display": "inline-block"});
+        $('#complete').fadeIn();
+        $(form).remove();
+      });
     });
   });
 
