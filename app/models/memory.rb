8bb832499f91ed5bf2cb19bfebebc95f2967f074
NatalieBlack <natalievictoriablack@gmail.com>
2015-11-30 00:05:39 -0500

    remove *latest_for_user methods from memory class and make *history_for methods stop once existing memories have been downloaded from APIs

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,14 +1,19 @@
 class Memory
   def self.history_for_user(u)
     Rails.logger.info "importing memories for #{u.name}"
-    twitter_history_for_user(u)
-    tumblr_history_for_user(u)
-    Rails.logger.info "done importing memories for #{u.name}"
+    tweets = twitter_history_for_user(u)
+    posts = tumblr_history_for_user(u)
+    memories = tweets + posts
+    Rails.logger.info "done importing #{memories.count} memories for #{u.name}"
+    return memories
   end
 
   def self.tumblr_history_for_user(u)
       Rails.logger.info "tumblr history for #{u.name}"
-      posts = collect_posts_with_offset do |offset|
+
+      created = []
+
+      posts = collect_posts_with_offset(u) do |offset|
         Rails.logger.info "calling tumblr API until #{offset}"
         options = {}
         options[:offset] = offset unless offset.nil?
@@ -16,7 +21,9 @@ class Memory
       end
 
       posts.each do |p|
+        break if u.tumblr_posts.find_by(tumblr_id: p['id'])
         Rails.logger.info "creating tumblr post #{p['id']}"
+
         new_post = TumblrPost.create({
           user_id: u.id,
           tumblr_id: p['id'],
@@ -29,13 +36,17 @@ class Memory
                 content: t
             })
         end
+
+        created << new_post
       end
-      return u.tumblr_posts
+      return created 
   end
 
   def self.twitter_history_for_user(u)
       Rails.logger.info "tumblr history for #{u.name}"
-      tweets = collect_tweets_with_max_id do |max_id|
+      created = []
+
+      tweets = collect_tweets_with_max_id(u) do |max_id|
           Rails.logger.info "calling twiter API until #{max_id}"
           options = {count: 200, include_rts: false}
           options[:max_id] = max_id unless max_id.nil?
@@ -43,8 +54,12 @@ class Memory
       end
       
       tweets.each do |tweet|
+
+        break if u.tweets.find_by(twitter_id: tweet.id.to_s)
+
         Rails.logger.info "creating tweet #{tweet.created_at}"
-          Tweet.create( {
+
+          created << Tweet.create( {
               user_id: u.id,
               time: tweet.created_at.to_datetime,
               text: tweet.full_text,
@@ -52,31 +67,7 @@ class Memory
               url: tweet.url
           })
       end
-      return u.tweets
-  end
-
-  def self.latest_for_user(u)
-    twitter_latest_for_user(u)
-    tumblr_latest_for_user(u)
-  end
-
-  def self.tumblr_latest_for_user(u)
-  end
-
-  def self.twitter_latest_for_user(u)
-      loop do
-          tweets = $twitter.user_timeline(u.twitter_name, {include_rts: false})
-          tweets.each do |tw|
-              break unless Tweet.create( {
-                  user_id: u.id,
-                  time: tweet.created_at.to_datetime,
-                  text: tweet.full_text,
-                  twitter_id: tweet.id,
-                  url: tweet.url
-              }) #stop looping if we aren't storing new tweets
-          end
-          break if u.tweets.find_by(twitter_id: tweets.last.id) #stop calling API if we got all the new tweets
-      end
+      return created 
   end
 
 private
@@ -84,17 +75,27 @@ private
     DateTime.strptime(timestamp_int.to_s, '%s')
   end
 
-  def self.collect_tweets_with_max_id(collection=[], max_id=nil, &block)
+  def self.collect_tweets_with_max_id(user, collection=[], max_id=nil, &block)
     response = yield(max_id)
     collection += response
-    response.empty? ? collection.flatten : collect_tweets_with_max_id(collection, response.last.id - 1, &block)
+    ids = response.map(&:id)
+    if response.empty? || user.tweets.where(twitter_id: ids).any?
+      collection.flatten
+    else
+      collect_tweets_with_max_id(user, collection, response.last.id - 1, &block)
+    end
   end
 
-  def self.collect_posts_with_offset(collection=[], offset=0, &block)
+  def self.collect_posts_with_offset(user, collection=[], offset=0, &block)
     response = yield(offset)
     collection += response
     new_offset = offset + 20
-    response.empty? ? collection.flatten : collect_posts_with_offset(collection, new_offset, &block)
+    ids = response.map { |p| p['id'] }
+    if response.empty? || user.tumblr_posts.where(tumblr_id: ids).any?
+      collection.flatten
+    else
+      collect_posts_with_offset(user, collection, new_offset, &block)
+    end
   end
 
 end
