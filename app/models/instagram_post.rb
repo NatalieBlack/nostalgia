f8a63174c74afcc58de9e78b7f81f37f8a686903
NatalieBlack <natalievictoriablack@gmail.com>
2015-12-20 22:13:21 -0500

    oops. proper class name

--- app/models/instagram_post.rb
+++ app/models/instagram_post.rb
@@ -1,6 +1,9 @@
-class Tweet < ActiveRecord::Base
+class InstagramPost < ActiveRecord::Base
   validates_uniqueness_of :instagram_id
   validates_presence_of :instagram_id, :time, :user_id, :url
 
   belongs_to :user
+
+  def display
+  end
 end
