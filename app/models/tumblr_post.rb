f39267849461f77477b0407fcdd136e6a9aa262f
NatalieBlack <natalievictoriablack@gmail.com>
2016-02-27 20:49:05 -0500

    update tumblr embed hack

--- app/models/tumblr_post.rb
+++ app/models/tumblr_post.rb
@@ -3,8 +3,8 @@ class TumblrPost < ActiveRecord::Base
 
   def display 
     html = HTTParty.get("#{url}/embed").body
-    embed_code = html.scan( /embed_key":"([^"]*)"/)[0][0]
-    embed_did = html.scan( /embed_did":"([^"]*)"/)[0][0]
+    embed_code = html.scan( /embed_key&quot;:&quot;([\w]*)&quot;/)[0][0]
+    embed_did = html.scan( /embed_did&quot;:&quot;([\w]*)&quot;/)[0][0]
     
     html = "<div class=\"tumblr_post_outer\"><div class=\"tumblr-post\" data-href=\"https://embed.tumblr.com/embed/post/#{embed_code}/#{tumblr_id}\" data-did=\"#{embed_did}\"><a href=\"#{url}\">#{url}</a></div><script async src=\"https://secure.assets.tumblr.com/post.js\"></script></div>"
     html.html_safe
